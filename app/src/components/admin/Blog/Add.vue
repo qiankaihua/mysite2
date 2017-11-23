a4769e5fc28919bca3d01280aefd8130e548d223
qiankaihua <qiankaihua@126.com>
2017-11-23 09:20:42 +0800

    back-up

---
+++ app/src/components/admin/Blog/Add.vue
@@ -0,0 +1,16 @@
+<template>
+  <!--富文本使用vue-UEeditor-->
+  <!--markdowm使用editor.md,vue-mdeditor,mavon-editor-->
+</template>
+
+<script>
+  export default {
+    name: '',
+    data: () => ({}),
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style scoped>
+</style>
